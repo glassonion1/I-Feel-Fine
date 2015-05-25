2b20e1647ef3836628c8ea47c8432900988cbc3c
Taisuke Fujita <glassonion999@gmail.com>
2015-05-25 23:04:51 +0900

    Fix compile error on TableView.

--- ios_swift/TableView/TableView/ViewController.swift
+++ ios_swift/TableView/TableView/ViewController.swift
@@ -41,7 +41,7 @@ class ViewController: UITableViewController {
     
     override func tableView(tableView: UITableView,
         cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath:indexPath) as UITableViewCell
+            let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath:indexPath) as! UITableViewCell
             cell.textLabel?.text = self.groups[indexPath.section][indexPath.row]
             return cell
     }
